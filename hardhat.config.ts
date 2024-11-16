5b4cdbd6d4c01d1b5fb9b0f5528dd6525088ca8b
yikkai <yikkai95@gmail.com>
2024-11-16 11:30:58 +0800

    fix: remove useless test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,16 @@ import "@nomicfoundation/hardhat-toolbox";
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
   networks: {
+    hardhat: {
+      accounts: [{
+        privateKey: process.env.WALLET!,
+        balance: "1000000000000000000000000"
+      }],
+      forking: {
+        blockNumber: 17978650,
+        url: process.env.BASE_SEPOLIA_RPC_URL || "https://sepolia.base.org", // https://docs.basescan.org/v/sepolia-basescan/
+      },
+    },
     baseSepolia: {
       url: process.env.BASE_SEPOLIA_RPC_URL || "https://sepolia.base.org", // https://docs.basescan.org/v/sepolia-basescan/
       accounts: [process.env.WALLET!],
--- test/MiracamNFT.ts
+++ test/MiracamNFT.ts
@@ -1,7 +1,9 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { Delta, Beta, Evan } from "../typechain-types";
 import ECDSA from 'ecdsa-secp256r1'
+import { MiracamNFT, MiracamNftMinter } from "../typechain-types";
+
+const AttesterAddress = "0xD798A4aDe873E2D447b43Af34e11882efEd911B1";
 
 async function extract(pubkey: string, signature: string) {
     const pubKeyBuf = Buffer.from(pubkey, 'base64')
@@ -37,9 +39,10 @@ function derToRS(der: Buffer) {
     return [r, s]
 }
 
-describe("Delta", function () {
-    let delta: Delta;
-
+describe("MiracamNFT", function () {
+    let miracamNFT: MiracamNFT;
+    let miracamNFTMinter: MiracamNftMinter;
+    
     beforeEach(async function () {
         // Deploy the Delta contract
                 
@@ -47,7 +50,7 @@ describe("Delta", function () {
         const miracamNFT = MiracamNFT.attach("0x4b79800e11fA527b01685056970D62878240Ea46") as MiracamNFT;
 
 
-        const miracamNFTMinter = await ethers.deployContract("MiracamNftMinter", [miracamNFT.target, AttesterAddress]);
+        miracamNFTMinter = await ethers.deployContract("MiracamNftMinter", [miracamNFT.target, AttesterAddress]);
         await miracamNFTMinter.waitForDeployment();
         console.log("miracamNFTMinter deployed to:", await miracamNFTMinter.getAddress());
 
@@ -66,15 +69,13 @@ describe("Delta", function () {
             //   const y = ethers.BigNumber.from("1357911131");
 
 
-            const proof =
-            {
-                "message": "owner=0x485fa1883a8ad8b09d69522a7f79162fc3eb8e80&url=https://devnet.irys.xyz/Hy2iM85af2poKdY59YhMhJviasEmKch6JspkRinaYR8G",
-                "owner": "0x485fa1883a8ad8b09d69522a7f79162fc3eb8e80",
-                "secp256r1_pubkey": "BALNspjpyeYwX9vArRtgHOOltyOYYMDDOiw3G6xiyAzEBrGfr6RXQp32J18GmvA+FifRGLGohYVdlisxZrPHD74=",
-                "signature": "MEUCIQCMEZ7cBKAbBwqpNJ2w1uX8TESccJmL3Dn8BDFw1L3jWQIgE+OpfU3qsTlUP2JqMDVFKwojWJouQ6kUjtQqGrxb94w=",
-                "url": "https://gateway.irys.xyz/CitxkaD3hLpTMaLkW1pagb1b6xPEtB6VvAjvfn6upyiZ"
-            }
-
+            const proof = {
+  "message": "owner=0xed3f8f972da9a0a3ffe13c0183d2326bcab969b3&url=https://gateway.irys.xyz/FztGVPqj2we61Fp8UBhnRJGDiayYpqihLsQAbCp2tMF4",
+  "owner": "0xed3f8f972da9a0a3ffe13c0183d2326bcab969b3",
+  "secp256r1_pubkey": "BGFVi/DhC12Vl/KqtWKdOptUNhxS/uJRUMvaNfWn9t3HGPrgUv4QqbO1056W94SzmAlF4gyUoptl85wuSPxGK7k=",
+  "signature": "MEUCIGQeTVFyaNaRPD3u37smiEwWFNsXNIQvqPvl4aHM3U9KAiEAnsI1FbTt2sDVTfROfQ/3AQkdBg+33YA6dAKeLZRfh+A=",
+  "url": "https://gateway.irys.xyz/FztGVPqj2we61Fp8UBhnRJGDiayYpqihLsQAbCp2tMF4"
+}
             // {
             //     "owner": "0x485fA1883A8ad8b09D69522A7f79162Fc3EB8E80",
             //     "url": "https://gateway.irys.xyz/EW2bRadrasSwaxzW1MfcAMYJmBbL1iGEwSHY2yCTJ5mJ",
@@ -122,7 +123,7 @@ describe("Delta", function () {
             // const to = ethers.getAddress("0x80a301ba2fb59c9a0e90616110bb39726643e1ce")
             // const uri = "https://example.com/token/1"
             // await delta.mint(extracted.hash, extracted.r, extracted.s, extracted.pubkey, to, url);
-            await delta.mint(extracted.r, extracted.s, extracted.pubkey, proof.owner, proof.url);
+            await miracamNFTMinter.mint(extracted.r, extracted.s, extracted.pubkey, proof.owner, proof.url);
             //   const result = await delta.verifySignature(extracted.hash, extracted.r, extracted.s, extracted.x, extracted.y, extracted.pubkey);
             //   
             // Note: This test will likely fail with these example values
@@ -130,48 +131,5 @@ describe("Delta", function () {
             //   expect(result).to.equal(true);
         });
 
-        // it.only("custom signature", async function () {
-
-        //     const pubkey = Buffer.from("04aa80d4d43513952023a54c3c6dc8216244b3fd0a2ba7bee4b2aaff72933ff2e9d63f3ec5254c3f81d2e51903a5e3b2e05f9c5d7c65d44608ee746ed3bba47672Ay59HrhovZQBRWiC7V545f7WknTNEjTuNF9hhlcIYsCl", 'hex').toString('base64')
-        //     const signature = Buffer.from("304402201c6edf533da960d889e4ac252c0a545c2e944395e074546125ccabe19506ca8c02206f63d6d9751317a0340fc62bbfa40ae051040a3ebdf911b20bcb342087b20db9", 'hex').toString('base64')
-        //     const { x, y, r, s } = await extract(pubkey, signature)
-        //     const message = "27f068a4e0742ff83d7758bd6144a890131ee9fb7373fb2b9c7afa99f0ef94f0"
-        //     const to = ethers.getAddress("0x80a301ba2fb59c9a0e90616110bb39726643e1ce")
-        //     const url = "https://devnet.irys.xyz/ZWopyQrLaLM9sjPY1D1UcqVcUEQ6GJYYPqsfBZsz3RJ"
-
-        //     // {
-        //     //     "url": "https://gateway.irys.xyz/3v6To64vFbB2LSxcxWw87TDvueHArnrNq8kXnjr2Q5vW",
-        //     //     "message": "owner=0x485fA1883A8ad8b09D69522A7f79162Fc3EB8E80&url=https://gateway.irys.xyz/3v6To64vFbB2LSxcxWw87TDvueHArnrNq8kXnjr2Q5vW",
-        //     //     "signature": "MEUCIFgVbyEFuLWIVwQ5eD+2npbdZQWY3d05DSZNlsaInS54AiEAgh3bfHelqgLE1XTH80JnUyW48tgd9Cnf1tLsmCkzhyI=",
-        //     //     "secp256r1_pubkey": "BALNspjpyeYwX9vArRtgHOOltyOYYMDDOiw3G6xiyAzEBrGfr6RXQp32J18GmvA+FifRGLGohYVdlisxZrPHD74=",
-        //     //     "owner": "0x485fA1883A8ad8b09D69522A7f79162Fc3EB8E80"
-        //     //   }
-
-        //     const hash = await crypto.subtle.digest("SHA-256", Buffer.from(message))
-        //     const extracted = {
-        //         hash: "0x" + Buffer.from(hash).toString('hex'),
-        //         x: "0x" + x.toString('hex'), 
-        //         y: "0x" + y.toString('hex'), 
-        //         r: "0x" + r.toString('hex'), 
-        //         s: "0x" + s.toString('hex'),
-        //         pubkey: Buffer.from(pubkey, 'base64')
-        //     } 
-
-        //     //  console.log(extracted)
-        //     // await delta.mint(extracted.hash, extracted.r, extracted.s, extracted.pubkey, to, url);
-        //     await delta.mint(extracted.r, extracted.s, extracted.pubkey, to, url);
-
-        // })
-
-        // it("should reject an invalid signature", async function () {
-        //   const hash = ethers.utils.keccak256("0x1234");
-        //   const r = ethers.BigNumber.from("0");
-        //   const s = ethers.BigNumber.from("0");
-        //   const x = ethers.BigNumber.from("0");
-        //   const y = ethers.BigNumber.from("0");
-
-        //   const result = await delta.verifySignature(hash, r, s, x, y);
-        //   expect(result).to.equal(false);
-        // });
     });
 });
